ba6f5db86a7708587931d0b9625b0233ab2eb54a
reinhardtcgr <reinhardt.cgr@gmail.com>
2015-12-29 00:08:40 -0500

    modularized the voting component into smaller components and wrote tests to make sure they are pure components

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+// Strictly speaking, 
+// the tests would start to pass if we only enabled the pure render mixin 
+// for Voting and didn't bother with the two other components. 
+// That's because when React doesn't notice any changes in the Voting props, 
+// it skips re-rendering for the whole component subtree
+// However, 
+// I find it more appropriate to consistently use the pure render mixin 
+// in all my components, both to make it explicit that the components are pure, 
+// and to make sure they will behave as I expect even after I rearrange them
+import Winner from './Winner';
+import Vote from './Vote';
 
 export default React.createClass({
-  getPair: function() {
-    return this.props.pair || [];
-  },
+  mixins: [PureRenderMixin],
   render: function() {
-    return <div className="voting">
-      {this.getPair().map(entry =>
-        <button key={entry}>
-          <h1>{entry}</h1>
-        </button>
-      )}
+    return <div>
+      {this.props.winner ?
+        <Winner ref="winner" winner={this.props.winner}/> :
+        <Vote {...this.props} />}
     </div>;
   }
 });
--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,7 @@ import Voting from './components/Voting';
 const pair = ['Trainspotting', '28 Days Later'];
 
 ReactDOM.render(
-  <Voting pair={pair} />,
+  <Voting pair={pair} winner="Trainspotting" />,
   document.getElementById('app')
 );
 
@@ -38,4 +38,21 @@ ReactDOM.render(
 // React components are just a stateless projection of the state 
 // at a given point in time
 
+
+// If we only use immutable data in component props, 
+// and write the component as a pure component, 
+// we can have React use a more efficient strategy for detecting changes in the props.
+
+// This is done by applying the PureRenderMixin 
+// that is available as an add-on package. 
+// When this mixin is added to a component, 
+// it changes the way React checks for changes in the component's props (and state). 
+// Instead of a deep compare it does a shallow compare, which is much, much faster
+
+// The reason we can do this is that by definition, 
+// there can never be changes within immutable data structures. 
+// If the props of a component are all immutable values, 
+// and the props keep pointing to the same values between renders, 
+// there can be no reason to re-render the component, 
+// and it can be skipped completely!
 
